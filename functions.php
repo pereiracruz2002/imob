9ec8661adac27804d70dcc20802bde09001780a2
flavio <flavio@wvtodoz.com>
2018-10-30 14:57:20 -0300

    load style.css

--- functions.php
+++ functions.php
@@ -1,9 +1,8 @@
 <?php
-
-load_imob_scripts(){
+function load_imob_scripts(){
+	wp_enqueue_style( 'imob_style', get_stylesheet_uri() );
 	wp_enqueue_style('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css', array( '' ), '3.3.7', 'all' );
-	wp_enqueue_style( 'template', get_stylesheet(), array( '' ), false, 'all' );
+	
 	wp_enqueue_script( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', array( 'jquery' ), null, true );
 }
-
 add_action( 'wp_enqueue_scripts', 'load_imob_scripts', 10, 1 );
